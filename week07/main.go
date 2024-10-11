0c0b8fdd5f9adfc46ddf85d730e2e02d355c29fe
cha80537 <cha80537@gmail.com>
2024-10-11 17:13:16 +0900

    v0.8 Stdin

--- week07/main.go
+++ week07/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt" // c language #include <stdio.h>
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-	//var now time.Time = time.Now()
-	//month := now.Month()
-	//fmt.Println(int(month))
-	//fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", now.Year(), int(now.Month()), now.Day())
-	//fmt.Printf("지금 시간은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
-	var army string = "우리는 국가와 국민에 충성을 다하는 대한민국 육군이다."
-	armyFixed := string.NewReplacer("!", "국")
-	fmt.Println(army)
-	fmt.Println(armyFixed.Replace(army))
+	fmt.Print("이름 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	//i, _ := r.ReadString('\n')
+	name, err := in.ReadString('\n')
+	fmt.Println(name)
+	fmt.Println(err)
 }
