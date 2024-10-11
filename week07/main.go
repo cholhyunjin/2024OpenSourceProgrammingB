cfe9588c72cfa986e06c0cf605c8116c2e614edf
cha80537 <cha80537@gmail.com>
2024-10-11 16:57:53 +0900

    v0.8 Replace

--- week07/main.go
+++ week07/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt" // c language #include <stdio.h>
-	"time"
 )
 
 func main() {
-	var now time.Time = time.Now()
+	//var now time.Time = time.Now()
 	//month := now.Month()
 	//fmt.Println(int(month))
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", now.Year(), int(now.Month()), now.Day())
-	fmt.Printf("지금 시간은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
+	//fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n", now.Year(), int(now.Month()), now.Day())
+	//fmt.Printf("지금 시간은 %d시 %d분 %d초 입니다\n", now.Hour(), now.Minute(), now.Second())
+	var army string = "우리는 국가와 국민에 충성을 다하는 대한민국 육군이다."
+	armyFixed := string.NewReplacer("!", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
 }
