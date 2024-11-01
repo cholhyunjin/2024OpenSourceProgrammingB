23d5101c566da391c77d92988d441f0611084787
cha80537 <cha80537@gmail.com>
2024-11-01 18:25:52 +0900

    v3.0 Prime number

--- week10/main.go
+++ week10/main.go
@@ -9,37 +9,42 @@ import (
 	"strings"
 )
 
+func isPrime(n int) bool {
+	//var isPrime bool = true
+	if n <= 1 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 { // 2를 제외한 모든 짝수는 소수가 아님
+		return false
+	} else { // 3이상의 홀수
+		i := 3
+		for i*i <= n {
+			if n%i == 0 {
+				//isPrime = false
+				//break
+				return false
+			}
+			fmt.Printf("%d", i) 
+			i = i + 2
+		}
+	}
+}
+
 func main() {
 	//fmt.Printf("%f\n", math.Sqrt(25.0))
 	fmt.Print("정수 입력 : ")
 	i := bufio.NewReader(os.Stdin)
 	number, err := i.ReadString('\n')
-	number = (strings.TrimSpace(number))
-	n, err := strconv.Atoi(number)
+	i := (strings.TrimSpace(number))
+	number, err := strconv.Atoi(number)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var isPrime bool = true
-	if n <= 1 {
-		isPrime = false
-	} else if n == 2 {
-		isPrime = true
-	} else if n%2 == 0 { // 2를 제외한 모든 짝수는 소수가 아님
-		isPrime = false
-	} else { // 3이상의 홀수
-		i := 3
-		//fot i < int(math.Sqrt(float64(n))) {
-		for i*i <= n {
-			if n%i == 0 {
-				isPrime = false
-				break // 1과 자기자신을 제외한 첫 번째 약수가 발견 되면 반복문 종료
-			}
-			fmt.Printf("%d", i) // 반복 횟수 확인용 코드
-			i++
-		}
+
 	}
-	if isPrime {
+	if isPrime(n) {
 		fmt.Printf("%d는 소수입니다.", n)
 	} else {
 		fmt.Printf("%d는 소수가 아닙니다.", n)
