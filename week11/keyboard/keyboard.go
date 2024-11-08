738d58541f738b2b90bcd78d95bcba507cc93735
cha80537 <cha80537@gmail.com>
2024-11-08 17:17:42 +0900

    v3.3 Prime number

---
+++ week11/keyboard/keyboard.go
@@ -0,0 +1,20 @@
+package keyboard
+
+import{
+
+}
+
+func GetInteger() (int, error) {
+	r := bufio.NewReader(os.Stdin)
+	a, err := r.ReadString('\n')
+	if err != nil {
+		//log.Fatal(err)
+		return 0, err
+	}
+	a = strings.TrimSpace(a)
+	n, err := strconv.Atoi(a)
+	if err != nil {
+		return 0, err
+	}
+	return n, nil
+}
